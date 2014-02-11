abae6ffbe9973d8bfe0ff9442bf71a473e8e1dfa
amamama <amtkrogzo@yahoo.co.jp>
2014-02-11 22:58:02 +0900

    tiny fix

--- main.c
+++ main.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "tweet.h"
-#include "webclient.h"
 #include <oauth.h>
 
+#include "tweet.h"
+#include "webclient.h"
+
 #include "keys/mykey.h"
-//#include "keys/soramimi_jp.h"
 
 int main(void)
 {
 	char *rep = NULL;
 	init_keys((union KEYS){.keys_array = {c_key, c_sec, t_key, t_sec}});
-	printf("%d\n",post_update("home_timelineに対応", &rep, 0, 0, (union L_L){.cood = {0, 0}}, 0, -1, -1));
+	printf("%d\n",post_update("status text", &rep, 0, 0, (union COOD){.cood = {0, 0}}, 0, -1, -1));
 	if(rep) {puts(rep);}
 	free(rep);
 	rep = NULL;
