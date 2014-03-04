9506b307a35f094645a7bd3d6c773b6f6325a537
amamama <amtkrogzo@yahoo.co.jp>
2014-03-04 09:54:29 +0900

    tiny fix

--- main.c
+++ main.c
@@ -12,11 +12,11 @@ int main(void)
 	char *rep = NULL;
 	init_keys((union KEYS){.keys_array = {c_key, c_sec, t_key, t_sec}});
 	curl_global_init(CURL_GLOBAL_DEFAULT);
-	printf("%d\n",post_update("status text", &rep, 0, 0, (struct GEOCODE){0, 0, 0, ""}, 0, -1, -1));
+	printf("%d\n",post_statuses_update("status text", &rep, 0, 0, (struct GEOCODE){0, 0, 0, ""}, 0, -1, -1));
 	if(rep) {puts(rep);}
 	free(rep);
 	rep = NULL;
-	printf("%d\n",get_home_timeline(&rep, 5, 0, 0, -1, -1, -1, -1) );
+	printf("%d\n",get_statuses_home_timeline(&rep, 5, 0, 0, -1, -1, -1, -1) );
 	if (rep){puts(rep);}
 	free(rep);
 	rep = NULL;
